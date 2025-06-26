bfa1e5b627e34cea1dcc83a141c51bc5f6e4e16d
Pablo <pablofermincordoba@gmail.com>
2025-06-26 20:33:59 -0300

    Ejercicio 2

---
+++ Ejercicio 2/ej2.js
@@ -0,0 +1,28 @@
+const lista = ["manzana","banana","pera","durazno","frutilla","mango"]
+
+const filtro = document.getElementById("filtro")
+
+filtro.addEventListener("click",(e)=>{
+    e.preventDefault()
+
+    const palabra = document.getElementById("text").value.trim().toLowerCase()
+    const error = document.getElementById("error")
+
+    if(palabra === ""){
+        error.innerText = "Error, ingrese una palabra"
+    }else{
+        error.innerText = ""
+    }
+
+    const filtrado = lista.filter((busqueda)=>{
+        return busqueda === palabra
+
+    })
+    
+    if(filtrado.length != 0){
+        const listaFiltrada = document.createElement("ul")
+        document.body.appendChild(listaFiltrada)
+        listaFiltrada.innerText = filtrado
+    }   
+    
+})
